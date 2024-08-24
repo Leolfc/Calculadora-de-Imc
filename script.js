88fb4df391b4e1e4091cdb7d2bc75ceb0cfcb464
Leolfc <147149946+Leolfc@users.noreply.github.com>
2024-08-24 15:05:18 -0300

    atualizei icone, estilo e o Javascript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let alt = document.getElementById("altura1");
 let pes = document.getElementById("peso1");
 let botao = document.querySelector("button");
-
+let div = document.getElementById('mostraResultado')
 function calculaImc(altura, peso) {
   return peso / (altura * altura);
 }
@@ -11,11 +11,11 @@ pes.focus();
 botao.addEventListener("click", function () {
   let imc = calculaImc(alt.value, pes.value);
   if (imc >= 18 && imc < 35) {
-    alert(`seu imc é de ${Math.round(imc)} .Esta excelente PARABÉNS!!!!!`);
-  } else if (imc < 18.5) {
-    alert("Você esta abaixo do peso!");
+  div.innerHTML=`<p>seu imc é de ${Math.round(imc)} .Esta excelente PARABÉNS!!!!!</p>`
+   } else if (imc < 18.5) {
+    div.innerHTML = "<p>Você esta abaixo do peso!</p>";
   } else if (imc > 35) {
-    alert("Você esta acima do peso!!");
+    div.innerHTML = "<p>Você esta acima do peso!!</p>";
   }
   alt.value = "";
   pes.value = "";
